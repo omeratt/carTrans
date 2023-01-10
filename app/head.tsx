f5dcdbfa9b1d2c1760f64ecd386488b40869dcd4
omeratt <omeratt1405@gmail.com>
2023-01-10 19:06:13 +0200

    header added

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Trans Car official</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.png" />
     </>
-  )
+  );
 }
